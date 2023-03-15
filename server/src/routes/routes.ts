0107fe3f8c7bbad05a05f8d9331891087fe16f78
JagathA <jagath.ariy@gmail.com>
2023-03-15 17:13:44 +0000

    feat: added /search

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as userController from "../controllers/user_controller";
 import * as recipeController from "../controllers/recipe_controller";
+import * as searchController from "../controllers/search_controller";
 
 export const router = express.Router();
 
@@ -8,3 +9,4 @@ router.post("/user", userController.createUser);
 router.get("/user", userController.getUser);
 
 router.get("/recipe", recipeController.getRecipe);
+router.get("/search", searchController.getRecipeList);
