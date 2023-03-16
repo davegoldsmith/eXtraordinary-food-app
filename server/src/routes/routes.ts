505ab40d8d2f178ea740bf6f3e51a5ed215cd6c5
Meenashi <jeyanthi_sm@yahoo.com>
2023-03-16 11:51:44 +0000

    Added mealPlanner endpoint in the router and removed mealPlanner seperate router

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as userController from "../controllers/user_controller";
 import * as recipeController from "../controllers/recipe_controller";
+import * as mealplanner_controller from "../controllers/mealplanner_controller";
 
 export const router = express.Router();
 
@@ -8,3 +9,6 @@ router.post("/user", userController.createUser);
 router.get("/user", userController.getUser);
 
 router.get("/recipe", recipeController.getRecipe);
+
+router.get("/DayMealPlanner", mealplanner_controller.getDayMealPlanner);
+router.get("/WeeklyMealPlanner", mealplanner_controller.getWeeklyMealPlanner);
