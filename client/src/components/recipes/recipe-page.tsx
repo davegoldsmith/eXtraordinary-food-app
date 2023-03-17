482d92099ab6c23bc376cd3c3a3373ee0a4fd03c
Nigel Faria <nigel.faria@gmail.com>
2023-03-17 21:32:03 +0000

    feat: View a recipe frontend Added route for recipe-page which requires a spoonacular recipe id to be passed in.

---
+++ client/src/components/recipes/recipe-page.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Container, CircularProgress } from '@mui/material';
+import RecipeComponent from './recipe';
+import { Recipe } from '../../types/recipe_types';
+import { useParams } from 'react-router-dom';
+
+const RecipePage: React.FC = () => {
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const {recipeId} = useParams();
+
+
+  useEffect(() => {
+    console.log(recipeId)
+    const fetchRecipe = async () => {
+      const response = await fetch(`http://localhost:3000/api/v1/recipe?recipeId=${recipeId}`);
+      const data = await response.json() as Recipe;
+      setRecipe(data);
+      setIsLoading(false);
+    };
+
+    fetchRecipe();
+  }, []);
+
+  return (
+    <Container maxWidth="md">
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        recipe && <RecipeComponent recipe={recipe} />
+      )}
+    </Container>
+  );
+};
+
+export default RecipePage;
