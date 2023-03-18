d33ac4159cf661ca4919cdb3d6ab5f818e2526f1
JagathA <jagath.ariy@gmail.com>
2023-03-18 02:10:06 +0000

    Fix : incorrect succesfull resp status code

--- server/src/controllers/search_controller.ts
+++ server/src/controllers/search_controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import * as searchService from "../services/search-service";
+import * as searchService from "../services/search_service";
 import {RecipeSearchParams} from '../types/search_types'
 
 
@@ -9,7 +9,7 @@ export const getRecipeList = async (req: Request, res: Response) => {
     const searchPara : RecipeSearchParams = req.query as RecipeSearchParams
     const recipelist = await searchService.getSearch(searchPara);
     //console.log("****** recipe list=>",recipelist );
-    res.status(201).json(recipelist);
+    res.status(200).json(recipelist);
   } catch (error) {
     let message = (error as Error).message;
     res.status(400).json({ message });
