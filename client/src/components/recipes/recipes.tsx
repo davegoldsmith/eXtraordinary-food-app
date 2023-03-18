98c2f083fad215fc64147ea00ad297a2e3ff5417
JagathA <jagath.ariy@gmail.com>
2023-03-18 23:58:57 +0000

    refactor : search result grid feat : filter side panel

--- client/src/components/recipes/recipes.tsx
+++ client/src/components/recipes/recipes.tsx
@@ -1,122 +1,114 @@
 import { useState, useEffect } from 'react';
-import { Select, TextField, AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Checkbox, FormControlLabel, Grid, Card, CardActionArea, CardMedia, CardContent } from '@mui/material';
+import { createTheme, ThemeProvider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Drawer, Box, CssBaseline, Select, TextField, AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Checkbox, FormControlLabel, Grid, Card, CardActionArea, CardMedia, CardContent } from '@mui/material';
 import { SearchResult, RecipeSearchResults } from '../../types/search_types';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
 
 
 const Recipes: React.FC = () => {
 
-    const [searchResult, setSearchResult] = useState<Array<SearchResult>>([]);
+    const theme = createTheme();
 
 
-    // function Navbar() {
-    //     const [anchorEl, setAnchorEl] = useState(null);
-    //     const [showFilters, setShowFilters] = useState(false);
+  const [searchResult, setSearchResult] = useState<Array<SearchResult>>([]);
+  const [sidePanelOpen, setSidePanelOpen] = useState(false);
+  const [cuisine, setCuisine] = useState("");
+  const [vegetarianOnly, setVegetarianOnly] = useState(false);
+  const [searchText, setSearchText] = useState("");
 
-    //     const handleMenuClick = (event) => {
-    //         setAnchorEl(event.currentTarget);
-    //     };
+  const fetchRecipe = async () => {
+    const response = await fetch(`http://localhost:3000/api/v1/search?`);
+    const data = await response.json();
+    console.log("****** client serach result =>", data.results);
+    setSearchResult(data.results);
+  };
 
-    //     const handleMenuClose = () => {
-    //         setAnchorEl(null);
-    //     };
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
 
-    //     const handleFilterToggle = () => {
-    //         setShowFilters(!showFilters);
-    //     };
+  const handleCuisineChange = (event) => {
+    setCuisine(event.target.value);
+  };
 
-    //     return (
-    //         <AppBar position="static">
-    //             <Toolbar>
+  const handleVegetarianOnlyChange = (event) => {
+    setVegetarianOnly(event.target.checked);
+  };
 
-    //                 <IconButton color="inherit" aria-label="filters" onClick={handleFilterToggle}>
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
 
-    //                 </IconButton>
-
-    //                     <div>
-    //                         <FormControlLabel control={<Checkbox />} label="Prime" />
-    //                         <FormControlLabel control={<Checkbox />} label="Sale" />
-    //                     </div>
-
-    //             </Toolbar>
-    //         </AppBar>
-    //     );
-    // }
-
-    function Navbar() {
-
-
-        return (
-            <Grid container direction="column">
-                <Checkbox />
-                <Select>
-                    <MenuItem>Option 1</MenuItem>
-                    <MenuItem>Option 2</MenuItem>
-                    <MenuItem>Option 3</MenuItem>
-                </Select>
-                <TextField label="Text Input" />
-            </Grid>
-        );
-    }
-
-
-    function ProductGrid() {
-        // const products = [
-        //     { name: 'Product 1', image: 'https://source.unsplash.com/random/300x200' },
-        //     { name: 'Product 2', image: 'https://source.unsplash.com/random/300x200' },
-        //     { name: 'Product 3', image: 'https://source.unsplash.com/random/300x200' },
-        //     { name: 'Product 4', image: 'https://source.unsplash.com/random/300x200' },
-        //     { name: 'Product 5', image: 'https://source.unsplash.com/random/300x200' },
-        //     { name: 'Product 6', image: 'https://source.unsplash.com/random/300x200' },
-        // ];
-
-        return (
-            <Grid container spacing={2}>
-                {searchResult.map((recipe) => (
-                    <Grid item xs={12} sm={6} md={4} key={recipe.id}>
-                        <Card>
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="140"
-                                    image={recipe.image}
-                                    alt={"Food"}
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {recipe.title}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Description of the product
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        );
-    }
-
-    useEffect(() => {
-
-        const fetchRecipe = async () => {
-            const response = await fetch(`http://localhost:3000/api/v1/search?`);
-            const data = await response.json() as RecipeSearchResults;
-            console.log("****** client serach result =>", data.results);
-            setSearchResult(data.results);
-            //setIsLoading(false);
-        };
-
-        fetchRecipe();
-    }, []);
-
-
-    return (
-        <>
-            <div>
-                <Navbar />
-                <ProductGrid />
-            </div>
-        </>);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <button onClick={() => setSidePanelOpen(true)}>Search Filter</button>
+      {/* <Typography variant="h1" align="center" sx={{ mb: 5 }}>
+        Recipe Search Results
+      </Typography> */}
+      <Grid container spacing={2} >
+        {searchResult.map((recipe) => (
+          <Grid item xs={12} sm={6} md={4} key={recipe.id}>
+            <Card sx={{ maxWidth: 345, height: "100%", mt: 5  }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={recipe.image}
+                  alt="Food"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {recipe.title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <Drawer anchor="right" open={sidePanelOpen} onClose={() => setSidePanelOpen(false)}>
+        <List sx={{ width: 250 }}>
+          <ListItem>
+            <ListItemText primary="Search Filters" />
+          </ListItem>
+          <ListItem>
+            <Select
+              label="Cuisine"
+              value={cuisine}
+              onChange={handleCuisineChange}
+              fullWidth
+            >
+              <MenuItem value="">Any Cuisine</MenuItem>
+              <MenuItem value="italian">Italian</MenuItem>
+              <MenuItem value="mexican">Mexican</MenuItem>
+              <MenuItem value="chinese">Chinese</MenuItem>
+            </Select>
+          </ListItem>
+          <ListItem>
+            <TextField
+              label="Search Text"
+              value={searchText}
+              onChange={handleSearchTextChange}
+              fullWidth
+            />
+          </ListItem>
+          <ListItem>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={vegetarianOnly}
+                  onChange={handleVegetarianOnlyChange}
+                  color="primary"
+                />
+              }
+              label="Vegetarian Only"
+            />
+          </ListItem>
+        </List>
+      </Drawer>
+     
+    </ThemeProvider>)
 };
 export default Recipes;
