197c0ad38498afa5671c5fa0eb13843cc63b0c12
Nigel Faria <nigel.faria@gmail.com>
2023-03-19 19:32:51 +0000

    feat: add loading wheel, error handling and empty url validation

--- client/src/components/recipes/add-recipe-page.tsx
+++ client/src/components/recipes/add-recipe-page.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Typography, TextField, Button } from "@mui/material";
+import { Typography, TextField, Button, CircularProgress } from "@mui/material";
 import { Recipe } from "../../types/recipe_types";
 import RecipeComponent from "./recipe";
 
 const AddRecipePage: React.FC = () => {
   const [recipeData, setRecipeData] = useState<Recipe | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -12,15 +14,29 @@ const AddRecipePage: React.FC = () => {
     const url = urlInput.value.trim();
 
     if (!url) {
+      setError("Please enter a valid URL.");
       return;
     }
 
+    setIsLoading(true);
+
     try {
       const response = await fetch(`http://localhost:3000/api/v1/recipe/ext?url=${url}`);
+
+      if (!response.ok) {
+        setError("An error occurred while fetching the recipe.");
+        setIsLoading(false);
+        return;
+      }
+
       const data = await response.json();
       setRecipeData(data as Recipe);
+      setError(null);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setError("An error occurred while fetching the recipe.");
+      setIsLoading(false);
     }
   };
 
@@ -38,9 +54,15 @@ const AddRecipePage: React.FC = () => {
           fullWidth
           margin="normal"
         />
+        {error && (
+          <Typography variant="body1" color="error" sx={{ mt: 1 }}>
+            {error}
+          </Typography>
+        )}
         <Button variant="contained" color="primary" type="submit">
           Add
         </Button>
+        {isLoading && <CircularProgress sx={{ ml: 2 }} />}
       </form>
       {recipeData && (
         <RecipeComponent recipe={recipeData} />
