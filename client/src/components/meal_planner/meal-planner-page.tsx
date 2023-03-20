8b85bd7c73be96b40bd0d4405bc91d1b49b92255
Nigel Faria <nigel.faria@gmail.com>
2023-03-20 16:58:39 +0000

    feat: circular loading

--- client/src/components/meal_planner/meal-planner-page.tsx
+++ client/src/components/meal_planner/meal-planner-page.tsx
@@ -4,6 +4,7 @@ import {
   FormLabel,
   Radio,
   RadioGroup,
+  CircularProgress
 } from "@mui/material";
 import { useState } from "react";
 import { DayMealPlan, WeekMealPlan } from "../../types/meal_planner_types";
@@ -12,6 +13,15 @@ import MealPlan from "./meal-planner";
 const MealPlanPage: React.FC = () => {
   const [mealPlanType, setMealPlanType] = useState<"day" | "week">("day");
   const [mealPlan, setMealPlan] = useState<DayMealPlan | WeekMealPlan>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleTimeFrameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMealPlanType(event.target.value as "day" | "week");
+    setIsLoading(true);
+    getMealPlan();
+    setIsLoading(false);
+  };
+
 
   const getMealPlan = async () => {
     let url = 'http://localhost:3000/api/v1/DayMealPlanner';
@@ -23,10 +33,6 @@ const MealPlanPage: React.FC = () => {
     setMealPlan(json);
   };
 
-  const handleTimeFrameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setMealPlanType(event.target.value as "day" | "week");
-    getMealPlan();
-  };
 
   return (
     <>
@@ -45,6 +51,7 @@ const MealPlanPage: React.FC = () => {
           <FormControlLabel value="week" control={<Radio />} label="week" />
         </RadioGroup>
       </FormControl>
+      {isLoading && <CircularProgress sx={{ ml: 2 }} />}
       {mealPlan && (<MealPlan mealData={mealPlan} timeFrame={mealPlanType} />)}
     </>
   );
